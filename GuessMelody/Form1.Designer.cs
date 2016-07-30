954073661c2c413c7479abb638b067616f80067a
rootkitlink <rootkitlink@mail.ru>
2016-07-30 20:06:18 +0300

    Start solution. Create class fMain.

---
+++ GuessMelody/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GuessMelody
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.btnGame = new System.Windows.Forms.Button();
+            this.btnParam = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGame
+            // 
+            this.btnGame.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGame.Location = new System.Drawing.Point(164, 46);
+            this.btnGame.Name = "btnGame";
+            this.btnGame.Size = new System.Drawing.Size(292, 51);
+            this.btnGame.TabIndex = 0;
+            this.btnGame.Text = "Играть";
+            this.btnGame.UseVisualStyleBackColor = true;
+            // 
+            // btnParam
+            // 
+            this.btnParam.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnParam.Location = new System.Drawing.Point(164, 104);
+            this.btnParam.Name = "btnParam";
+            this.btnParam.Size = new System.Drawing.Size(292, 51);
+            this.btnParam.TabIndex = 1;
+            this.btnParam.Text = "Настройки";
+            this.btnParam.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(164, 161);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(292, 51);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GuessMelody.Properties.Resources.brainmusic;
+            this.ClientSize = new System.Drawing.Size(624, 343);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnParam);
+            this.Controls.Add(this.btnGame);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fMain";
+            this.Text = "Угадай мелодию";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGame;
+        private System.Windows.Forms.Button btnParam;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
